adb0330b40c0a52f18661e02499008dbe1d4c3e8
James Woods <james.woods396@gmail.com>
2019-12-22 03:37:56 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/scribblink/smartie-vessel-service
+
+go 1.13
+
+require (
+	github.com/DataDog/zstd v1.4.4 // indirect
+	github.com/golang/protobuf v1.3.2
+	github.com/micro/go-micro v1.18.0
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.2.0
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"log"
+
+	"github.com/micro/go-micro"
+	pb "github.com/scribblink/smartie-vessel-service/proto/vessel"
+)
+
+const (
+	defaultHost = "datastore:27017"
+)
+
+func main() {
+	srv := micro.NewService(
+		micro.Name("shippy.service.vessel"),
+	)
+
+	srv.Init()
+
+	uri := os.Getenv("DB_HOST")
+	if uri == "" {
+		uri = defaultHost
+	}
+	client, err := CreateClient(uri)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer client.Disconnect(context.TODO())
+
+	vesselCollection := client.Database("shippy").Collection("vessel")
+	repository := &VesselRepository{
+		vesselCollection,
+	}
+
+
+	// Register our implementation with
+	pb.RegisterVesselServiceHandler(srv.Server(), &handler{repository})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
